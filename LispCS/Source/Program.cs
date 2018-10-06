3a56a679c985022c7b5e6f7b241e775849af4a7b
Alfantin <mstplus@gmail.com>
2018-10-06 15:13:27 -0300

    LispCS

---
+++ LispCS/Source/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace Source
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ LispCS/Test/UnitTest1.cs
@@ -0,0 +1,15 @@
+using System;
+using Xunit;
+
+namespace Test {
+
+    public class UnitTest1 {
+
+        [Fact]
+        public void Test1() {
+
+        }
+
+    }
+
+}
