cf09095aaa97aca35c6ce33d3d5a808d8d76b87c
Evren Yortuçboylu <yortucboylu@gmail.com>
2018-10-14 20:57:14 +0200

    Parse comment, literal

--- LipsCS/Source/Asts/List.cs
+++ LipsCS/Source/Asts/List.cs
@@ -15,5 +15,4 @@ public class List : Ast {
         }
         return ret;
     }
-
 }
--- LipsCS/Source/Parser.cs
+++ LipsCS/Source/Parser.cs
@@ -1,4 +1,6 @@
-﻿namespace Source {
+﻿using Source.Tokens;
+
+namespace Source {
 
     public class Parser {
 
@@ -9,6 +11,25 @@
         }
 
         public Ast Read() {
+            if(Lexer.Peek() is TokenComment || Lexer.Peek() is TokenMultilineComment){
+                Lexer.Read();
+            }
+
+            if(Lexer.Peek() is TokenNumber) {
+                var numberToken = (TokenNumber)Lexer.Read();
+                return new Literal(numberToken.Value);
+            }
+
+            if(Lexer.Peek() is TokenBool){
+                var boolToken = (TokenBool)Lexer.Read();
+                return new Literal(boolToken.Value);
+            }
+
+            if(Lexer.Peek() is TokenString){
+                var stringToken = (TokenString)Lexer.Read();
+                return new Literal(stringToken.Value);
+            }
+
             return null;
         }
 
